3dc4b6502efad4793b9ca5267864b0dc977c744f
RED22 <1120359682@qq.com>
2022-11-03 11:59:02 +0800

    hotfix  分支添加

--- pro-rabbitmq/pro-rabbitmq-consumer/src/main/java/com/atguigu/test/Test.java
+++ pro-rabbitmq/pro-rabbitmq-consumer/src/main/java/com/atguigu/test/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("hotfix1.0");
+        System.out.println("分支添加");
     }
 }
