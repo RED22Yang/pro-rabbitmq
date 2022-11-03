e8f1c0952dd2517c0a1145069e3faa0a1f3ab3dd
RED22 <1120359682@qq.com>
2022-11-03 10:50:28 +0800

    更新3

--- pro-rabbitmq/pro-rabbitmq-consumer/src/main/java/com/atguigu/test/Test.java
+++ pro-rabbitmq/pro-rabbitmq-consumer/src/main/java/com/atguigu/test/Test.java
@@ -4,5 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("更新1");
         System.out.println("更新2");
+        System.out.println("更新3");
+
     }
 }
